4e1fcd9bbbcc07ae2cadff40b7feab3fb46dcefd
zzb <40760215+zzbuzzard@users.noreply.github.com>
2023-11-27 22:21:11 +0000

    Big improvement to L2 guidance

--- sd_l2_guidance.py
+++ sd_l2_guidance.py
@@ -27,28 +27,21 @@ class GuidedImageToImage(nn.Module):
 
         self.guidance_strength = guidance_strength
 
-    def forward(self, xs):
+    def forward(self, xs, **kwargs):
         with torch.no_grad():
             xs_p = self.sd.image_processor.preprocess(xs)
             xs_p = xs_p.half()
             goal_latents = self.sd.vae.encode(xs_p).latent_dist.sample()
             goal_latents = goal_latents * self.sd.vae.config.scaling_factor
 
+        # Callback for L2-guidance during DDIM
         def callback(pipe, step_index, timestep, kwargs):
             latents = kwargs["latents"]
-            with torch.enable_grad():
-                latents = torch.nn.Parameter(latents)
-                loss = (latents - goal_latents).pow(2).mean()
-                loss.backward()
-                g = latents.grad
-
-            den = torch.sqrt(1 - pipe.scheduler.alphas_cumprod[step_index])
-            num = 1 - pipe.scheduler.alphas[step_index]
-            mul = 1 / torch.sqrt(pipe.scheduler.alphas[step_index])
-            s = self.guidance_strength * mul * num / den
-
-            latents -= s * g
-            kwargs["latents"] = latents.data
+            noise_pred = kwargs["noise_pred"]
+            grads = (latents - goal_latents) / ((latents - goal_latents).pow(2).sum())
+            mul = torch.sqrt(1 - pipe.scheduler.alphas_cumprod[timestep])
+            noise_pred = noise_pred - self.guidance_strength * mul * grads
+            kwargs["noise_pred"] = noise_pred
             return kwargs
 
         # xs : B x C x H x W
@@ -62,7 +55,8 @@ class GuidedImageToImage(nn.Module):
             return_dict=False,
             output_type="pt",  # return a Tensor
             callback_on_step_end=callback,
-            callback_on_step_end_tensor_inputs=['latents']
+            callback_on_step_end_tensor_inputs=['latents', 'noise_pred'],
+            **kwargs
         )
         # Given batch size 1, the pipeline automatically removes this dimension
         if len(out.shape) == 3:
@@ -83,12 +77,19 @@ if __name__ == "__main__":
     transform = util.eval_transform(size=512)
     test_ds = NoisyDataset(root_path=args.directory, split="test", transform=transform)
 
+    # By default, Stable Diffusion uses PNDM scheduler.
+    # Here I use DDIM, and copy the following parameters from the default Stable Diffusion scheduler:
+    scheduler = diffusers.DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear",
+                                        clip_sample=False, set_alpha_to_one=False)
+
     sd = diffusers.StableDiffusionImg2ImgPipeline.from_pretrained(
         args.model,
         torch_dtype=torch.float16,
-        safety_checker=None,
+        safety_checker=None
     ).to(device)
+    sd.scheduler = scheduler
     # sd.enable_freeu(s1=0.9, s2=0.2, b1=1.2, b2=1.4)
+    sd.enable_xformers_memory_efficient_attention()
     sd.set_progress_bar_config(disable=True)
 
     model = GuidedImageToImage(sd,
@@ -99,6 +100,5 @@ if __name__ == "__main__":
                                nsteps=args.nsteps,
                                guidance_strength=args.guidance_strength).to(device)
 
-    out_dir = f"out/l2guidedsd_s={args.strength}_cfg={args.cfg}_gs={args.guidance_strength}"
+    out_dir = f"out/l2guidedsd_ddim_s={args.strength}_cfg={args.cfg}_gs={args.guidance_strength}"
     util.inference_on_dataset(model, test_ds, out_dir, batch_size=1)
-
